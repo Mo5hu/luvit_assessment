486ed98d5528e0d1e9799a81cf13cdd233026c23
mo5hu <k173634@nu.edu.pk>
2023-09-09 03:10:44 +0500

    Feat: Api Service Added

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -5,7 +5,8 @@ class ApiService {
 
   Future<dynamic> resetData() async {
     final response = await http.post(Uri.parse(baseUrl));
-
+    print(
+        'debugging: Reset Firebase POST request statusCode: ${response.statusCode}');
     if (response.statusCode == 201) {
       return response.body;
     } else {
